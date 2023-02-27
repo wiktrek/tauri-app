c7c8360cb447d7c6a20d784164538eac056b0e24
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-27 20:37:47 +0100

    tailwind works

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,35 @@
     windows_subsystem = "windows"
 )]
 
+use std::time::{Instant, Duration };
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+fn start(start: bool) -> bool {
+    format!("DZIAŁA {}", start);
+    println!("{}", start);
+    if start == true {
+        return false
+    } else {
 
+        return true
+    }
+
+
+}
+#[tauri::command]
+fn timer(start: bool) -> Duration {
+    let now = Instant::now();
+    format!("DZIAŁA / timer");
+    if start == true {
+return Duration::new(0, 0);
+    } else {
+        return now.elapsed()
+    }
+}
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![start, timer])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,30 +3,21 @@ import { invoke } from '@tauri-apps/api/tauri';
 import Image from 'next/image';
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState('');
-  const [name, setName] = useState('');
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke('greet', { name }));
+  const [start, setStart] = useState(false);
+  const [response, setResponse] = useState(false);
+  async function start_stop() {
+    setStart(await invoke('start', { start: start }));
+    if (start === true) {
+      timer();
+    }
   }
-
+  async function timer() {
+    setResponse(await invoke('timer', { start: start }));
+  }
   return (
-    <div className="h-10 bg-slate-700">
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <a className=" text-red-800">wrfa</a>
+    <div className="h-screen bg-[#181a1b] text-white">
+      <button onClick={start_stop}>{start ? 'stop' : 'start'}</button>
+      <p>{response}</p>
     </div>
   );
 }
