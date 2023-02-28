b392e6294c735387068447a10c1473259fd735a0
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-28 23:25:46 +0100

    idk how to make this work

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,7 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
-
+use std::thread::sleep;
 use std::time::{Instant, Duration };
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -11,13 +11,17 @@ fn start(start: bool) -> u64{
 
     format!("DZIAŁA {}", start);
     println!("{}", start);
-
-    let now = Instant::now();
-    stoptimer(now)
+    seconds();
+    return 1232131
 }
-fn stoptimer(timer: Instant) -> u64{
-return timer.elapsed().as_secs()
+fn seconds() {
+    let mut a = 0;
 
+    loop {
+sleep(Duration::from_secs(1));
+a += 1;
+println!("{}", a);
+    }
 }
 fn main() {
     tauri::Builder::default()
