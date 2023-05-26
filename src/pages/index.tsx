3a3f1dfcc64ea4a9f2f559b051354a46c5e8b690
wiktrek <wiktor.janiszewski10@gmail.com>
2023-05-26 23:38:39 +0200

    display a random text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,20 +3,27 @@ import { invoke } from '@tauri-apps/api/tauri';
 import Image from 'next/image';
 
 function App() {
-const [texts, setTexts] = useState([""])
-
-// change text every 5 minutes
-function change_text() {
-
-
+  const [texts, setTexts] = useState(['example', 'example 2']);
+  const [displayed, setDisplayed] = useState('');
+  // change text every 5 minutes
+  function changeText() {
+    console.log(texts.length);
+    setDisplayed(texts[getRandomText(texts.length)]);
+    // setNewText('test');
   }
 
+  function getRandomText(max: number) {
+    return Math.floor(Math.random() * max);
+  }
 
-
+  function setNewText(text: string) {
+    setTexts([...texts, text]);
+  }
   return (
     <div className="h-screen bg-[#181a1b] text-white">
-   <p>change text every 5 minutes</p>
-      <button>click me!</button>
+      <p>change text every 5 minutes</p>
+      <p className="text-4xl">Text: {displayed}</p>
+      <button onClick={changeText}>click me!</button>
     </div>
   );
 }
