929053415ffb21bdbfb340f4010c6869e00bee4f
wiktrek <wiktor.janiszewski10@gmail.com>
2023-05-27 00:00:14 +0200

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,24 +5,49 @@ import Image from 'next/image';
 function App() {
   const [texts, setTexts] = useState(['example', 'example 2']);
   const [displayed, setDisplayed] = useState('');
+  // most used english  words according to wikipedia.com
+  let exampleTexts = [
+    'the',
+    'be',
+    'to',
+    'of',
+    'and',
+    'a',
+    'in',
+    'that',
+    'have',
+    'I',
+    'for',
+    'not',
+  ];
+
   // change text every 5 minutes
   function changeText() {
-    console.log(texts.length);
-    setDisplayed(texts[getRandomText(texts.length)]);
+    const random = getRandomText(texts.length);
+    let text = texts[random];
+    if (text === displayed) return changeText();
+    setDisplayed(text);
+
     // setNewText('test');
   }
 
   function getRandomText(max: number) {
-    return Math.floor(Math.random() * max);
+    let number = Math.floor(Math.random() * max);
+    console.log(number, max, texts);
+    return number;
   }
 
-  function setNewText(text: string) {
-    setTexts([...texts, text]);
-  }
+  // function setNewText() {
+  //   setTexts([...texts, ...exampleTexts]);
+  //   // setTexts([...texts, text]);
+  // }
+  /* <button onClick={setNewText}>wiktrek</button> */
+
   return (
     <div className="h-screen bg-[#181a1b] text-white">
       <p>change text every 5 minutes</p>
       <p className="text-4xl">Text: {displayed}</p>
+
       <button onClick={changeText}>click me!</button>
     </div>
   );
